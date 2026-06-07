dcf2d5229035ef11ac95ff7a353b31e89a67bc36
H5misi <hazm.sayed.m@gmail.com>
2026-06-07 16:25:16 +0300

    Initial commit

---
+++ src/test/java/com/soundcloud/SoundCloudUsingSpringBoot/SoundCloudUsingSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soundcloud.SoundCloudUsingSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoundCloudUsingSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
